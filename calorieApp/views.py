40c958bf0d6290001d968c35ea8a6a64cb1b485a
thatiah <thatiahnia@gmail.com>
2026-07-05 14:04:34 +0300

    add function for adding foods and their calories

--- calorieApp/views.py
+++ calorieApp/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from .models import *
+from django.contrib import messages
 
 # Create your views here.
+
+def add_food(request):
+    if request.method == "post":
+        name = request.POST["food_name"]
+        calories = request.POST["food_calories"]
+        if name and calories:
+            Food.objects.create(food_name=name , food_calories=calories)
+            return redirect("food_list")
+
+    all_food = Food.objects.all()
+    total_calories = sum(f.calories for f in all_food)
+    return render(request , food_list.html)
