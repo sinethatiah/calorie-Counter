37c0d068549202f75ca219879d0cb0ced29f4341
thatiah <thatiahnia@gmail.com>
2026-07-05 09:05:31 +0300

    create class for food and make migrations

--- calorieApp/models.py
+++ calorieApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Food(models.Model):
+    food_name=models.CharField()
+    food_calories=models.FloatField()
+    def __str__(self):
+        return f"{self.food_name}"
