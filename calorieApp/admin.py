8b84728a2c49344699435133ee7b12a6e59d6175
thatiah <thatiahnia@gmail.com>
2026-07-16 00:35:20 +0300

    register models on admin.py

--- calorieApp/admin.py
+++ calorieApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Food
 
 # Register your models here.
+@admin.register(Food)
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ("food_name", "food_calories")
+    search_fields = ("food_name",)
