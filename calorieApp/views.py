8a46d772311cc16b1d266df233e0ba6558ff6f57
thatiah <thatiahnia@gmail.com>
2026-07-15 20:33:45 +0300

    reconfigure views to suit the new food form

--- calorieApp/views.py
+++ calorieApp/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render , redirect
 from .models import *
 from django.contrib import messages
+from .forms import FoodForm
 
 # Create your views here.
 
 def add_food(request):
-    if request.method == "post":
-        name = request.POST["food_name"]
-        calories = request.POST["food_calories"]
-        if name and calories:
-            Food.objects.create(food_name=name , food_calories=calories)
-            return redirect("food_list")
+    if request.method == "POST":
+        form = FoodForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("add_food")
+    else:
+        form = FoodForm()
 
     all_food = Food.objects.all()
     total_calories = sum(f.calories for f in all_food)
-    return render(request , "food_list.html")
+    return render(request, "calorie_app/food_list.html", {
+        "form": form,
+        "foods": all_food,
+        "total_calories": total_calories,
+    })
+
+def reset_foods(request):
+    Food.objects.all().delete()
+    return redirect("add_food")
+
+        
